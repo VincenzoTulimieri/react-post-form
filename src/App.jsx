daec98c6f03f4283aa107888d5e54937e692526b
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-04-15 22:35:48 +0200

    feat: creato form con gli input

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,55 @@
-
+// importazione file
+import { useState } from "react";
 
 
 function App() {
 
+
+
+
+
   return (
     <>
-      <h1>react form</h1>
+      <header>
+        <h1 className="title-page">Inserisci il tuo post</h1>
+      </header>
+      <main>
+        <div className="container">
+          <form>
+            <div className="input-container">
+              <label>Autore</label>
+              <input
+                type="text"
+                name="author"
+                required
+              />
+            </div>
+            <div className="input-container">
+              <label>Titolo</label>
+              <input
+                type="text"
+                name="title"
+                required
+              />
+            </div>
+            <div className="input-container">
+              <label>Testo</label>
+              <textarea
+                name="body"
+                required
+              />
+            </div>
+            <div className="input-container check">
+              <input
+                type="checkbox"
+                name="public"
+              />
+              <label>Rendi pubblico</label>
+            </div>
+            <button className="btn" type="submit">Invia il tuo post</button>
+          </form>
+        </div>
+      </main>
     </>
   )
 }
